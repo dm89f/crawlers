d850f9ea770476c88dbd3e989589b8582cfb223e
Dileep B C <dileep@figmatic.co>
2023-05-28 13:56:43 +0530

    refactored files

--- src/website_dataset/square_space_no_sitemap.js
+++ src/website_dataset/square_space_no_sitemap.js
@@ -1,1 +1,6 @@
-[{"url":"http://www.fireandflourbread.com/"},{"url":"https://www.craig-reynolds.com/"},{"url":"https://omakase.coolhunting.com/"},{"url":"https://creative.squarespace.com/"},{"url":"https://beautifuldestinations.com/"},{"url":"https://www.lafayettesquare.com/"},{"url":"https://www.lucabeltrame.com/"},{"url":"https://g.page/my-codeless-website?share"},]
+let data_set = [{ "url": "http://www.fireandflourbread.com/" }, { "url": "https://www.craig-reynolds.com/" }, { "url": "https://omakase.coolhunting.com/" }, { "url": "https://creative.squarespace.com/" }, { "url": "https://beautifuldestinations.com/" }, { "url": "https://www.lafayettesquare.com/" }, { "url": "https://www.lucabeltrame.com/" }, { "url": "https://g.page/my-codeless-website?share" }]
+
+
+module.exports = {
+  data_set
+}
