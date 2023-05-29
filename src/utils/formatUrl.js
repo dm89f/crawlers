e4d2309660b1453138b80f3264ab3cee42eff914
Dileep B C <dileep@figmatic.co>
2023-05-29 14:38:00 +0530

    fix:formatted urls

--- src/utils/formatUrl.js
+++ src/utils/formatUrl.js
@@ -3,14 +3,14 @@ const { InvalidUrlError } = require("../CustomErrors/CustomErrors");
 const hasTLD = (url) => {
   const parsedURL = new URL(url);
   const hostnameParts = parsedURL.hostname.split('.');
-  return hostnameParts.length > 2;
+  if (hostnameParts[0] === 'www')
+    return hostnameParts.length > 2;
+  else return hostnameParts.length >= 2;
 }
-
 
 const formatUrlString = (urlStr = "") => {
 
   let newUrlStr = urlStr.toLowerCase();
-
   //add https:// as the default protocol if the Url doesnt have one
   if (!newUrlStr.startsWith('https://') && !newUrlStr.startsWith('http://')) {
     newUrlStr = 'https://' + newUrlStr;
@@ -22,7 +22,7 @@ const formatUrlString = (urlStr = "") => {
     let hostname = urlInput.hostname;
     let protocol = urlInput.protocol;
 
-    formtdUrl = protocol + "//" + urlInput.hostname;
+    formtdUrl = protocol + "//" + urlInput.hostname
 
     if (!hasTLD(formtdUrl))
       throw new Error(`Entered url ${urlStr} does not contain TLD`)
