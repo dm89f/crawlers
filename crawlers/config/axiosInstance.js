62e9e4678cfd8222dbddab6bc2227815366bd0a6
Dileep B C <dileep@figmatic.co>
2023-05-30 17:30:26 +0530

    added saas landing page

--- crawlers/config/axiosInstance.js
+++ crawlers/config/axiosInstance.js
@@ -8,7 +8,7 @@ Axios.interceptors.request.use((config) => {
   // Modify headers to mimic Chrome browser
   config.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36';
   config.headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
-  config.headers['timeout'] = '15000'
+  config.headers['timeout'] = '15000';
   return config;
 });
 
