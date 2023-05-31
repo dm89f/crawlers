060dcb0c1cf782cd912d9ccb47a1e3580022b306
Dileep B C <dileep@figmatic.co>
2023-06-01 02:36:01 +0530

    tested

--- test_page_tree.js
+++ test_page_tree.js
@@ -3,10 +3,6 @@ const axios = require('axios');
 let node_cnt = 0;
 const static_urls = new Set();
 const template_urls = new Map();
-const http = require('http');
-const https = require('https');
-
-
 
 let isNodeRoot = (node) => {
   return node.id === '000000';
@@ -22,22 +18,6 @@ const generateId = () => {
   return '0'.repeat(10 - idString.length) + idString;
 }
 
-async function getLinkIfResponseOk(pageUrl) {
-
-  try {
-    const response = await axios.get(pageUrl, { timout: 5000 });
-    if (response.status >= 200 && response.status < 300) {
-      return { page_exist: true, error: null };
-    } else {
-      throw new Error("Page Does not Exist");
-    }
-  } catch (error) {
-    console.log(error);
-    return { page_exist: false, error: error.message };
-  }
-
-}
-
 function isSegmentDynamic(segment) {
   return !isNaN(segment) || segment.length <= 2;
 }
@@ -49,8 +29,6 @@ const getUrlOrigin = (url = "") => {
 
 }
 
-
-
 const addUrlToTree = async (root_node, url) => {
 
   // console.log("adding", url);
@@ -61,7 +39,10 @@ const addUrlToTree = async (root_node, url) => {
   for (let idx = 0; idx < segments.length; ++idx) {
 
     let segment = segments[idx];
-    if (isSegmentDynamic(segment)) continue;
+    if (isSegmentDynamic(segment) && (idx !== (segments.length - 1))) {
+      console.log("continue", segment, idx)
+      continue
+    };
     path += `/${segment}`;
     let node_url = getUrlOrigin(url) + path;
 
@@ -71,40 +52,34 @@ const addUrlToTree = async (root_node, url) => {
         if (isNodeRoot(node)) {
           static_urls.add(path);
         } else {
-          let temp_path = path.substring(0, path.length - segment.length - 1);
+          let temp_path = path.substring(0, path.length - segment.length - 1);  //template path
           let nodeArr = template_urls.get(temp_path);
-          template_urls.set(temp_path, [...nodeArr, path])
+          if (nodeArr) {
+            template_urls.set(temp_path, [...nodeArr, path])
+          } else {
+            template_urls.set(temp_path, [path]);
+          }
         }
-        childNode = {
-          id: generateId(),
-          name: segment,
-          path: path,
-          url: node_url,
-          parentId: node.parent_id,
-          children: [],
-        }
-        node.children.push(childNode);
+
       } else {
         template_urls.set(path, []);
         let status = await pingUrl(node_url);
         if (status) {
           static_urls.add(path);
         } else {
-          console.log(status)
-          console.log("stat", node_url)
+          console.log("not reachable", node_url)
         }
-        childNode = {
-          id: generateId(),
-          name: segment,
-          path: path,
-          url: node_url,
-          parentId: node.parent_id,
-          children: [],
-        }
-        node.children.push(childNode);
+
       }
-
-
+      childNode = {
+        id: generateId(),
+        name: segment,
+        path: path,
+        url: node_url,
+        parentId: node.parent_id,
+        children: [],
+      }
+      node.children.push(childNode);
     }
 
     node = childNode;
@@ -117,7 +92,7 @@ async function pingUrl(url) {
     const response = await axios.head(url);
     return response.status >= 200 && response.status < 400; // Webpage exists if status is in the 2xx or 3xx range
   } catch (error) {
-    console.log(error);
+    console.log(error.response.status);
     return false; // Error occurred or webpage does not exist
   }
 }
@@ -137,50 +112,94 @@ let test = async () => {
     children: [],
   }
 
-  let urls = ['https://ruya.ae/',
-
-    'https://ruya.ae/work/ritz-carlton/',
-    'https://ruya.ae/work/emirates-nation-brand/',
-    'https://ruya.ae/work/federal-youth-authority/',
-    'https://ruya.ae/work/piramal-mahalaxmi/',
-    'https://ruya.ae/work/al-habtoor-city/',
-    'https://ruya.ae/work/melia-group-of-hotels/',
-    'https://ruya.ae/work/nurai/',
-    'https://ruya.ae/work/lapita/',
-    'https://ruya.ae/work/chaimaa-avenue/',
-    'https://ruya.ae/work/al-erkyah-city/',
-    'https://ruya.ae/work/npcc/',
-    'https://ruya.ae/work/mbl/',
-    'https://ruya.ae/work/mag-city/',
-    'https://ruya.ae/work/mag-corporate/',
-    'https://ruya.ae/work/dusit-thani-beachfront-sri-lanka/',
-    'https://ruya.ae/work/the-fields-at-mbr-city/',
-    'https://ruya.ae/work/five-jumeirah-village/',
-    'https://ruya.ae/work/five-palm-jumeirah/',
-    'https://ruya.ae/work/dlf-5/',
-    'https://ruya.ae/work/zaya-corporate/',
-    'https://ruya.ae/work/hameni/',
-    'https://ruya.ae/work/ghreiwati-group/',
-    'https://ruya.ae/work/the-crest/',
-    'https://ruya.ae/work/mina-by-azizi/',
-    'https://ruya.ae/work/texture/',
-    'https://ruya.ae/work/verde/',
-    'https://ruya.ae/work/ariane/',
-    'https://ruya.ae/work/ariane-city/',
-    'https://ruya.ae/work/talex/',
-    'https://ruya.ae/work/castel-royale/',
-    'https://ruya.ae/work/provedore/',
-    'https://ruya.ae/services/3d-design-agency-in-dubai/',
-    'https://ruya.ae/services/digital-web-design-agency-in-dubai/',
-    'https://ruya.ae/services/branding-agency-in-dubai/',
-    'https://ruya.ae/careers/business-development-executive/',
-    'https://ruya.ae/careers/graphic-designer/',
-    'https://ruya.ae/legal/',
-    'https://ruya.ae/connect/',
-    'https://ruya.ae/careers/',
-    'https://ruya.ae/services/',
-    'https://ruya.ae/branding-digital-marketing-agency/',
-    'https://ruya.ae/work/',
+  let urls = [
+    'https://rnsit.ac.in/',
+    'https://rnsit.ac.in/mech/',
+    'https://rnsit.ac.in/mca/',
+    'https://rnsit.ac.in/ise/',
+    'https://rnsit.ac.in/eie/',
+    'https://rnsit.ac.in/cse-data-science/',
+    'https://rnsit.ac.in/eee/',
+    'https://rnsit.ac.in/ece/',
+    'https://rnsit.ac.in/cse/',
+    'https://rnsit.ac.in/civil/',
+    'https://rnsit.ac.in/aiml/',
+    'https://rnsit.ac.in/nirf/',
+    'https://rnsit.ac.in/naac-file/',
+    'https://rnsit.ac.in/admissions/',
+    'https://rnsit.ac.in/placements/',
+    'https://rnsit.ac.in/campus-life/',
+    'https://rnsit.ac.in/about/',
+    'https://rnsit.ac.in/admin-staff/',
+    'https://rnsit.ac.in/chem/chem-staff/',
+    'https://rnsit.ac.in/chem/chem-facilities-infrastructure/',
+    'https://rnsit.ac.in/chem/chem-conferences-and-projects/',
+    'https://rnsit.ac.in/chem/',
+    'https://rnsit.ac.in/physics/physics-staff/',
+    'https://rnsit.ac.in/physics/physics-facilities-infrastructure/',
+    'https://rnsit.ac.in/physics/physics-conferences-and-projects/',
+    'https://rnsit.ac.in/physics/',
+    'https://rnsit.ac.in/math/staff/',
+    'https://rnsit.ac.in/math/',
+    'https://rnsit.ac.in/math/math-staff/',
+    'https://rnsit.ac.in/mca/mca-student-life/',
+    'https://rnsit.ac.in/mca/mca-staff/',
+    'https://rnsit.ac.in/mca/mca-facilities-infrastructure/',
+    'https://rnsit.ac.in/mca/mca-conferences-and-projects/',
+    'https://rnsit.ac.in/mca/mca-placement-statistics/',
+    'https://rnsit.ac.in/cybersecurity/',
+    'https://rnsit.ac.in/cse/cse-staff/',
+    'https://rnsit.ac.in/cse/cse-placement-statistics/',
+    'https://rnsit.ac.in/cse/cse-facilities-infrastructure/',
+    'https://rnsit.ac.in/cse/cse-student-life/',
+    'https://rnsit.ac.in/cse/cse-conferences-and-projects/',
+    'https://rnsit.ac.in/cse-data-science/staff/',
+    'https://rnsit.ac.in/cse-data-science/facilities-infrastructure/',
+    'https://rnsit.ac.in/aiml/aiml-student-life/',
+    'https://rnsit.ac.in/aiml/aiml-conferences-and-projects/',
+    'https://rnsit.ac.in/aiml/aiml-facilities-infrastructure/',
+    'https://rnsit.ac.in/aiml/aiml-staff/',
+    'https://rnsit.ac.in/civil/civil-student-life/',
+    'https://rnsit.ac.in/civil/civil-facilities-infrastructure/',
+    'https://rnsit.ac.in/civil/civil-conferences-and-projects/',
+    'https://rnsit.ac.in/civil/civil-placement-statistics/',
+    'https://rnsit.ac.in/civil/civil-staff/',
+    'https://rnsit.ac.in/mech/mech-student-life/',
+    'https://rnsit.ac.in/mech/mech-conferences-and-projects/',
+    'https://rnsit.ac.in/mech/mech-facilities-infrastructure/',
+    'https://rnsit.ac.in/mech/mech-placement-statistics/',
+    'https://rnsit.ac.in/mech/mech-staff/',
+    'https://rnsit.ac.in/eie/eie-student-life/',
+    'https://rnsit.ac.in/eie/eie-staff/',
+    'https://rnsit.ac.in/eie/eie-conferences-and-projects/',
+    'https://rnsit.ac.in/eie/eie-facilities-infrastructure/',
+    'https://rnsit.ac.in/eie/eie-placement-statistics/',
+    'https://rnsit.ac.in/eee/eee-student-life/',
+    'https://rnsit.ac.in/eee/eee-conferences-and-projects/',
+    'https://rnsit.ac.in/eee/eee-facilities-infrastructure/',
+    'https://rnsit.ac.in/eee/eee-placement-statistics/',
+    'https://rnsit.ac.in/eee/eee-staff/',
+    'https://rnsit.ac.in/ise/ise-student-life/',
+    'https://rnsit.ac.in/ise/ise-placement-statistics/',
+    'https://rnsit.ac.in/ise/ise-conferences-and-projects/',
+    'https://rnsit.ac.in/ise/ise-facilities-infrastructure/',
+    'https://rnsit.ac.in/ise/ise-staff/',
+    'https://rnsit.ac.in/ece/ece-student-life/',
+    'https://rnsit.ac.in/ece/ece-conferences-and-projects/',
+    'https://rnsit.ac.in/ece/ece-staff/',
+    'https://rnsit.ac.in/ece/ece-placement-statistics/',
+    'https://rnsit.ac.in/ece/ece-facilities-infrastructure/',
+    'https://rnsit.ac.in/sports/',
+    'https://rnsit.ac.in/cultural/',
+    'https://rnsit.ac.in/contact-us/',
+    'https://rnsit.ac.in/cse-placement-statistics',
+    'https://rnsit.ac.in/naac-dvv/',
+    'https://rnsit.ac.in/ece-placement-statistics',
+    'https://rnsit.ac.in/eee-placement-statistics',
+    'https://rnsit.ac.in/cse-data-science/cse-staff/',
+    'https://rnsit.ac.in/eie-placement-statistics',
+    'https://rnsit.ac.in/ise-placement-statistics',
+    'https://rnsit.ac.in/mca-placement-statistics'
   ];
 
   console.time("start-time")
@@ -190,7 +209,7 @@ let test = async () => {
   }
 
   console.timeEnd("start-time")
-  // fs.writeFileSync('sample_tree.json', JSON.stringify(root_node));
+  fs.writeFileSync('sample_tree.json', JSON.stringify(root_node));
   let calc_tot_urls = 0;
 
   for (let [temp, arr] of template_urls.entries()) {
@@ -199,6 +218,7 @@ let test = async () => {
       calc_tot_urls += arr.length;
     }
   }
+  console.log(template_urls.keys())
   console.log(urls.length, static_urls.size, calc_tot_urls)
 
 }
